c3e55e9b4f189a6d3e223bfbe7722bb1f238754b
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-04-29 17:04:56 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const signUp = document.getElementById("sign-up")
 const hidebtn = document.getElementById("symbol")
-const singUpbtn = document.getElementById("sign-up-btn")
+const formData = document.getElementById("card") 
+const submitbtn = document.getElementById("submit")
+let newData = new FormData(formData)
 setTimeout(function(){
     signUp.style.display = "flex"
 }, 3000)
 hidebtn.addEventListener("click", function(){
     signUp.style.display = "none"
 })
-singUpbtn.addEventListener("click" ,function(){
-    signUp.style.display = "flex"
-} )
+submitbtn.addEventListener("click" , function(){
+    console.log(newData)
+    name = newData.get("firstName")
+    signUp.innerHTML = `
+    <h1>Thank you ${name} </h1>
+    <p>we are loading the page now .....`
+    setTimeout(function(){
+        signUp.innerHTML = `<h1>Successful sign up Thank you`
+        
+
+    } , 1500)
+    
+
+
+})
+
